0a8f77c6013ff04eb1c099ae0a42a99ad378565d
Shivwhoo <shivamkishore009@gmail.com>
2026-01-06 00:25:14 +0530

    added subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -4,16 +4,63 @@ import { Subscription } from "../models/subscription.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { use } from "react"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    
+
+    const userId=await User.findById(req.user._id)
+    const channel = await User.findById(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    if(userId.toString()=== channelId.toString()){
+        throw new ApiError(403,"You cannot subscribe to yourself")
+    }
+
+    const existingSubscription = await Subscription.findOne({
+        subscriber: userId,
+        channel: channelId
+    })
+
+    if (existingSubscription) {
+        // unsubscribe
+        await Subscription.deleteOne({
+            _id: existingSubscription._id
+        })
+
+        return res.status(200).json(
+            new ApiResponse(200, null, "Unsubscribed successfully")
+        )
+    }
+
+    const newSubscription = await Subscription.create({
+        subscriber: userId,
+        channel: channelId
+    })
+    return res.status(200)
+    .json(new ApiResponse(200,newSubscription,"Subscribed successfully"))
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    const channel = await User.findById(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    const subscription=await Subscription.find({channel:channelId})
+
+    const totalSubscriber= await Subscription.countDocuments({channel:channelId})
+
+    return res.status(200).
+    json(new ApiResponse(200,subscription,"Subs fetched successfully"))
 })
 
 // controller to return channel list to which user has subscribed
