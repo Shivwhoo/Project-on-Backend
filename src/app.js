28d91759c6df9bf58f8ee3c3122a562ecf676d72
Shivwhoo <shivamkishore009@gmail.com>
2026-01-07 16:42:11 +0530

    completed the controller and routes

--- src/app.js
+++ src/app.js
@@ -18,9 +18,18 @@ app.use(cookieParser())
 
 
 //routes import
-import userRouter from "./routes/user.routes.js"
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
-routes declaration
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
@@ -30,10 +39,6 @@ app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 
-
 //http:localhost:8000/api/v1/users/_____
-
-
-
 
 export {app}
